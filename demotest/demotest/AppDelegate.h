be0cef478ff94641d0c6cabeafc624281e953b1c
ys329527677 <329527677@qq.com>
2016-04-28 15:46:14 +0800

    first ys

---
+++ demotest/demotest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demotest
+//
+//  Created by 杨帅 on 16/4/28.
+//  Copyright © 2016年 杨帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
